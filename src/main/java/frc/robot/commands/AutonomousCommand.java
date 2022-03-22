48cb1f6399273eb633019a963006a97821cf5224
Isaac Patton <23pattisa@isd748.org>
2022-03-22 17:11:06 -0500

    just tuning pid dont merge to master

--- src/main/java/frc/robot/commands/AutonomousCommand.java
+++ src/main/java/frc/robot/commands/AutonomousCommand.java
@@ -31,7 +31,7 @@ public class AutonomousCommand extends SequentialCommandGroup {
 
     public AutonomousCommand(DriveTrain drive, Intake intake, FlyWheel fly) {
         super ( //new AutoIntake(intake, Constants.intakeSpinSpeed, timer, 13, 12),
-                new AutoDriveDistance(drive)
+                new AutoDrivePID(drive)
                /* new AutoIntake(intake, Constants.intakeSpinSpeed, 8.5, 7.5), 
                 new AutoDrive(drive, Constants.autoDriveSpeed * -1, timer,7, 4.5), 
                 new AutoScore(fly, Constants.dumpSpeed, timer, 4,2)*/);
