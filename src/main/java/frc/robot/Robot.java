5a0e60d850ccf4ca363970ed50e50d17953a59df
Cameron Huse <camjams101@yahoo.com>
2022-03-24 18:02:00 -0500

    fixed tank drive

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -114,6 +114,7 @@ public class Robot extends TimedRobot {
 
     @Override
     public void teleopInit() {
+        RobotContainer.frontLeftDriveMotor2.setSelectedSensorPosition(0);
         // This makes sure that the autonomous stops running when
         // teleop starts running. If you want the autonomous to
         // continue until interrupted by another command, remove
