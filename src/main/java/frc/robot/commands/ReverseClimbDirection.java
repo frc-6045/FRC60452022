77044a3ce0aff4313a87e5d2b0d78abfa8b9ca98
Cameron Huse <camjams101@yahoo.com>
2022-03-25 17:24:07 -0500

    fixed reverse climb

--- src/main/java/frc/robot/commands/ReverseClimbDirection.java
+++ src/main/java/frc/robot/commands/ReverseClimbDirection.java
@@ -6,11 +6,15 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
+import frc.robot.subsystems.Lift;
 
 public class ReverseClimbDirection extends CommandBase {
+  private final Lift m_lift;
   /** Creates a new ReverseLiftDirection. */
-  public ReverseClimbDirection() {
+  public ReverseClimbDirection(Lift subsystem) {
+   m_lift = subsystem; 
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(m_lift);
   }
 
   // Called when the command is initially scheduled.
@@ -20,12 +24,14 @@ public class ReverseClimbDirection extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    Constants.climbDirection *= -1;
+    m_lift.getLiftMotor().set(Constants.liftMotorSpeed * -1);
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    m_lift.getLiftMotor().set(0);
+  }
 
   // Returns true when the command should end.
   @Override
