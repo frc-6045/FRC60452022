fc179dacf1fd56501fd007a23209a07603a4cf09
Cameron Huse <camjams101@yahoo.com>
2022-04-05 18:18:25 -0500

    Merge Isaac with main

--- src/main/java/frc/robot/commands/TankDrive.java
+++ src/main/java/frc/robot/commands/TankDrive.java
@@ -54,15 +54,10 @@ public class TankDrive extends CommandBase {
         //m_driveTrain.getDifferentialDrive().tankDrive(leftJoystick.getY() * -Constants.DriveSpeed * driveScale, rightJoystick.getY() * Constants.DriveSpeed * driveScale);
      //double turningValue = (Constants.kAngleSetPoint -RobotContainer.gyro.getAngle()) * Constants.kP;
        // turningValue = Math.copySign(turningValue, leftJoystick.getY(), rightJoystick.getY());
-<<<<<<< HEAD
-       m_driveTrain.getDifferentialDrive().tankDrive(leftJoystick.getY() * -Constants.DriveSpeed * Constants.driveDirection , rightJoystick.getY() * Constants.DriveSpeed * Constants.driveDirection);
-    //System.out.println(RobotContainer.frontLeftDriveMotor2.getSelectedSensorPosition());
-=======
         m_driveTrain.my_TankDrive(leftJoystick.getY() * Constants.DriveSpeed, rightJoystick.getY() * Constants.DriveSpeed);
         System.out.println(driveScale);
    
        System.out.println(m_driveTrain.get_Right_Encoder_inch());
->>>>>>> origin/Isaac
     }
 
   
