216926b31daea5ebdd5dc8610dd40f269fdb733d
Sabre Robotics <sabrerobotics6045@gmail.com>
2022-04-07 10:07:52 -0500

    auto drive update

--- src/main/java/frc/robot/commands/AutoDriveGyro.java
+++ src/main/java/frc/robot/commands/AutoDriveGyro.java
@@ -18,10 +18,11 @@ public class AutoDriveGyro extends CommandBase {
   private double m_heading;
 private final PIDController m_PIDHeading;
 private double m_DistanceSetPoint;
+
   
   public AutoDriveGyro(DriveTrain subsystem, double speed, double distanceSetPoint, double heading) {
     // Use addRequirements() here to declare subsystem dependencies.
-    m_PIDHeading = new PIDController(0.05 , 0, 0);
+    m_PIDHeading = new PIDController(0 , 0, 0);
     m_PIDHeading.setTolerance(1);
     m_PIDHeading.enableContinuousInput(-180, 180);
     m_driveTrain = subsystem;
@@ -48,25 +49,12 @@ private double m_DistanceSetPoint;
 
   double turningValue = m_driveTrain.getHeading();
   double PIDOut = m_PIDHeading.calculate(turningValue);
-  double maximumMotorOutput = 0.3;
-  double minimumMotorOutput = -0.3;
-  double motorOutput;
-  if (PIDOut <= maximumMotorOutput && PIDOut <= minimumMotorOutput) {
-  motorOutput = PIDOut;
-  } else if ( PIDOut >= maximumMotorOutput) {
-  motorOutput = minimumMotorOutput;
-  } else if ( PIDOut <= minimumMotorOutput) {
-  motorOutput = maximumMotorOutput;
-  } else {
-    motorOutput = 0.1;
-  }
-
-
   // turningValue = Math.copySign(turningValue, speed);
-  m_driveTrain.getDifferentialDrive().arcadeDrive(m_speed, motorOutput); 
-  System.out.println("motorOutput" + motorOutput);
-  System.out.println("PIDOutput" + PIDOut);
-  System.out.println("Gyro value" + m_driveTrain.getHeading());
+
+
+  m_driveTrain.getDifferentialDrive().arcadeDrive(m_speed, PIDOut);  
+  System.out.println("Gyro value: " + m_driveTrain.getHeading());
+  System.out.println();
   
 
  }
@@ -81,6 +69,6 @@ private double m_DistanceSetPoint;
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return m_PIDHeading.atSetpoint() && Math.abs(m_driveTrain.get_Right_Encoder_inch()) >= m_DistanceSetPoint;
+    return /*m_PIDHeading.atSetpoint() && */ Math.abs(m_driveTrain.get_Right_Encoder_inch()) >= m_DistanceSetPoint;
   }
 }
